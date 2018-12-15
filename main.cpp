120256da05a0102f73bfadee51fdebc784d1f979
predatorlypony <43167413+predatorlypony@users.noreply.github.com>
2018-12-15 20:38:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -33,11 +33,6 @@ int Database_user_2[M][N];
 2nd line: remaining shots
 3rd and 4th lines: position of element
 */
-void getTheMapFromTheFile(char MapMapArray[][H][W], string mapFileName);
-void drowTheMap(char MapMapArray[][H][W]);
-void addEconomics(char MapArray[][H][W], int resourse1Amount1stp = 111, int resourse2Amount1stp = 99, int resourse1Amount2ndp = 77, int resourse2Amouny2ndp = 4); // default values are spubs
-void fillIndexesInTheMAp(char MapArray[][H][W]);
-void addGunsList(char MapArray[][H][W], int gunSet = 1, int gunCost1 = 50, int gunCost2 = 10); // 1 is the stub
 
 void main()
 {
