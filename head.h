4f39f48626908ff252e5182f3f9365242213fb5a
predatorlypony <43167413+predatorlypony@users.noreply.github.com>
2018-12-15 20:38:56 +0200

    Update head.h

--- head.h
+++ head.h
@@ -3,4 +3,8 @@
 #include<ctime>
 #include <fstream>
 
-void testFunction();
+void getTheMapFromTheFile(char MapMapArray[][H][W], string mapFileName);
+void drowTheMap(char MapMapArray[][H][W]);
+void addEconomics(char MapArray[][H][W], int resourse1Amount1stp = 111, int resourse2Amount1stp = 99, int resourse1Amount2ndp = 77, int resourse2Amouny2ndp = 4); // default values are spubs
+void fillIndexesInTheMAp(char MapArray[][H][W]);
+void addGunsList(char MapArray[][H][W], int gunSet = 1, int gunCost1 = 50, int gunCost2 = 10); // 1 is the stub
