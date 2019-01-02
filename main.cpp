40f7f161252a4f21b8d90faeff572b7b2d826d24
Xaldej <45791928+Xaldej@users.noreply.github.com>
2019-01-02 10:06:12 +0200

    Revert "mignatenko"
    This reverts commit 81f06c6ad4d66c517bb7d8ac9cf01c75c88c3ef9.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*JoombrBoo
+authours:
+alexey melentyev
+mihail ignatenko
+oleg sazonov
+date: 10.12.18*/
+
+#include"head.h"
+
+void main()
+{
+	char Map[I][H][W];
+	bool doContinue = true;
+	getTheMapFromTheFile(Map, firstMap);
+	addEconomics(Map);
+	addGunsList(Map);
+	fillIndexesInTheMAp(Map);
+	drowTheMap(Map);
+	while (doContinue)
+	{
+
+	}
+	return;
+}
